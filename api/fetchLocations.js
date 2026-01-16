f591a0612f66de81c03a3389b40b96a72c6430f8
Kunal Kumar <4002kunal88@gmail.com>
2026-01-16 23:46:24 +0530

    till -16jan 2026

--- api/fetchLocations.js
+++ api/fetchLocations.js
@@ -1,9 +1,10 @@
-const LOCATION_ENDPOINT = "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa1paaXJvQTFhalFZdHZyX0lFRGZkcnotZVdMd3xBQ3Jtc0tuMVJQYTFjZEpEblhMb2syZ01iYWpELUN3TlYxS2lMMll5S1Y1T3hqUjdGV0d5dWFsZWh4TGlTVFoxdF83Y3pZa0VnVGZycGpKV0pHb25YczVuRUl2RC1iQ1NiSnZVZlJqcW9PTkVTLXNBVm0zTGl6NA&q=https%3A%2F%2Fttp.cbp.dhs.gov%2Fschedulerapi%2Flocations%2F%3Ftemporary%3Dfalse%26inviteOnly%3Dfalse%26operational%3Dtrue%26serviceName%3DGlobal%2520Entry&v=U4R-ABnzfHA"
+const LOCATION_ENDPOINT = "https://ttp.cbp.dhs.gov/schedulerapi/locations/?temporary=false&inviteOnly=false&operational=true&serviceName=Global+Entryredirect?event=video_description&redir_token=QUFFLUhqa1paaXJvQTFhalFZdHZyX0lFRGZkcnotZVdMd3xBQ3Jtc0tuMVJQYTFjZEpEblhMb2syZ01iYWpELUN3TlYxS2lMMll5S1Y1T3hqUjdGV0d5dWFsZWh4TGlTVFoxdF83Y3pZa0VnVGZycGpKV0pHb25YczVuRUl2RC1iQ1NiSnZVZlJqcW9PTkVTLXNBVm0zTGl6NA&q=https%3A%2F%2Fttp.cbp.dhs.gov%2Fschedulerapi%2Flocations%2F%3Ftemporary%3Dfalse%26inviteOnly%3Dfalse%26operational%3Dtrue%26serviceName%3DGlobal%2520Entry&v=U4R-ABnzfHA"
 
 export default function fetchLocations(){
     fetch(LOCATION_ENDPOINT)
         .then( response => response.json())
         .then( data => {
+            const filteredLocations = 
             console.log(data);
         })
         .catch(error => {
